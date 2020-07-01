66d2017454c28bb146da1223df40ec32b84380e2
Igor Alimov <igoral@gmail.com>
2020-07-01 13:13:39 +0300

    Add sync for input signals and registers for output signals

--- soda_machine.sv
+++ soda_machine.sv
@@ -14,57 +14,86 @@ module soda_machine(
 	typedef enum logic [2:0] {S0, S1, S2, S3, S4} state_type;
 	(* syn_encoding = "default" *) state_type state, nextstate;
 	
+	insert_type insert_sync0, insert_sync1;
+	logic pour_water_out, change1_out, change2_out, change22_out;
+	
 	always_ff @(posedge clk, posedge reset)
-	if (reset)
+	if (reset) begin
 		state <= S0;
-	else
+		insert_sync0 <= I0;
+		insert_sync1 <= I0;
+		pour_water <= 0;
+		change1 <= 0;
+		change2 <= 0;
+		change22 <= 0;
+	end else begin
 		state <= nextstate;
+		insert_sync0 <= insert;
+		insert_sync1 <= insert_sync0;
+		pour_water <= pour_water_out;
+		change1 <= change1_out;
+		change2 <= change2_out;
+		change22 <= change22_out;
+	end
 	
 	always_comb
 		case (state)
 			S0:
-				case (insert)
+				case (insert_sync1)
 					I1:
 						nextstate = S1;
 					I2:
 						nextstate = S2;
 					I5:
 						nextstate = S0;
+					default:
+					   nextstate = S0;
 				endcase
 			S1: 
-				case (insert)
+				case (insert_sync1)
 					I1:
 						nextstate = S2;
 					I2:
 						nextstate = S3;
 					I5:
 						nextstate = S0;
+					default:
+					   nextstate = S1;
 				endcase
 			S2:
-				case (insert)
+				case (insert_sync1)
 					I1:
 						nextstate = S3;
 					I2: 
 						nextstate = S4;
 					I5:
 						nextstate = S0;
+					default:
+					   nextstate = S2;
 				endcase
-			S3: 
-				if (insert == I1)
+			S3:
+				case (insert_sync1)
+					I0:
+						nextstate = S3;
+					I1:
+						nextstate = S4;
+					default:
+						nextstate = S0;
+				endcase
+			S4:
+				if (insert_sync1 == I0)
 					nextstate = S4;
 				else
 					nextstate = S0;
-			S4:
-				nextstate = S0;
 		endcase
 		
-	assign pour_water = (state == S4) | (insert == I5) | (state == S3) & (insert == I2);
+	assign pour_water_out = (state == S4) | (insert_sync1 == I5) | (state == S3) & (insert_sync1 == I2);
 	
-	assign change1 = (state == S1) & (insert == I5) | (state == S3) & (insert == I5) | (state == S4) & (insert == I2);
+	assign change1_out = (state == S1) & (insert_sync1 == I5) | (state == S3) & (insert_sync1 == I5) | (state == S4) & (insert_sync1 == I2);
 							
-	assign change2 = (state == S2) & (insert == I5) | (state == S3) & (insert == I5);
+	assign change2_out = (state == S2) & (insert_sync1 == I5) | (state == S3) & (insert_sync1 == I5);
 	
-	assign change22 = (state == S4) & (insert == I5);
+	assign change22_out = (state == S4) & (insert_sync1 == I5);
 	
 endmodule
 	
--- soda_machine_types.sv
+++ soda_machine_types.sv
@@ -2,7 +2,7 @@
 
 package soda_machine_types;
 
-	typedef enum logic [1:0] {I1=2'b01, I2=2'b10, I5=2'b11} insert_type;
+	typedef enum logic [1:0] {I0=2'b00, I1=2'b01, I2=2'b10, I5=2'b11} insert_type;
 	
 endpackage
 
